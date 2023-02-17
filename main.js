29e18f3226608693757df7c8cbb2aaaec0f89859
Edith Misik <105500352+emisik@users.noreply.github.com>
2023-02-17 18:53:08 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,69 @@
+//practice on Scope
+function calc() {
+  let extra = 5;
+  let sum = function (a, b) {
+    return a + b;
+  };
+
+  let total = extra + sum(2, 3);
+  console.log(extra);
+  console.log(total);
+}
+
+//String Methods
+function solution(str, ending) {
+  if (str.endsWith(ending)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(solution("abc", "bc"));
+console.log(solution("abc", "d"));
+
+//String Methods
+function getCount(str) {
+  let string = 0;
+  let vowels = "aeiou";
+  let lower = str.toLowerCase();
+  for (let i = 0; i < lower.length; i++) {
+    if (vowels.includes(lower[i])) {
+      string++;
+    }
+  }
+  return string;
+}
+
+console.log(getCount("comeingae"));
+
+//Use of built in objects
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
+
+//Use .replace () method to remove vowes in a string
+
+function remove(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
+console.log(remove("whatcanbedone"));
+
+//Use of reduce method
+function XO(str) {
+  let lower = str.toLowerCase();
+  let split = lower.split("");
+
+  let reducex = split.reduce((n, value2) => {
+    return n + (value2 === "x");
+  }, 0);
+
+  let reducey = split.reduce((n, value2) => {
+    return n + (value2 === "o");
+  }, 0);
+
+  if (reducex === reducey) {
+    return true;
+  } else {
+    return false;
+  }
+}
